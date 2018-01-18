3a1386b4d3b0673fcc326a543e9339c589819eb3
Kelsey Pintens <ksepintens@gmail.com>
2018-01-18 15:38:21 -0600

    complete exercise

---
+++ reindeer.js
@@ -0,0 +1,11 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var hohohoElement = document.getElementById("reindeer");
+
+
+for (let loopTracker = 0; loopTracker < reindeer.length; loopTracker += 1) {
+
+hohohoElement.innerHTML += colors[loopTracker] + " " + reindeer[loopTracker] + "</br>";
+}
